8a1dc5f6c618203791fb365d3edf7c0aead82034
Bystroushaak <bystrousak@kitakitsune.org>
2020-01-12 16:13:54 +0100

    Recompiled. Image overlay should work now. #24

--- twitter_script.js
+++
@@ -1,9 +0,0 @@
-function add_twitter_link() {
-    var blog_url = encodeURIComponent(document.location);
-    var blog_title = encodeURIComponent(document.title);
-    var url = "https://twitter.com/intent/tweet?text=" + blog_title + "&url=" + blog_url + "&via=Bystroushaak";
-
-    var twitter_link = document.getElementById("twitter_button");
-    twitter_link.href = url;
-    twitter_link.style.visibility = "visible";
-}
